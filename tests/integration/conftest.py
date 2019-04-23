f7076a2d3072447719f728caadf08f33a6a26a7a
Simon Brunning <simon@brunningonline.net>
2019-04-23 15:15:13 +0100

    Use contextlib.closing to ensure db connection closed.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,7 @@
 # encoding=utf-8
 import logging
 import sqlite3
+from contextlib import closing
 from io import StringIO
 
 import pytest
@@ -10,19 +11,18 @@ logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope="package")
 def db():
-    conn = sqlite3.connect(":memory:")
+    with closing(sqlite3.connect(":memory:")) as conn:
+        cursor = conn.cursor()
 
-    cursor = conn.cursor()
-    cursor.execute(
-        "CREATE TABLE sausages (kind VARCHAR NOT NULL PRIMARY KEY, rating INT NOT NULL);"
-    )
-    cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("cumberland", 10))
-    cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("vegetarian", 0))
-    cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("lincolnshire", 9))
-    conn.commit()
+        cursor.execute(
+            "CREATE TABLE sausages (kind VARCHAR NOT NULL PRIMARY KEY, rating INT NOT NULL);"
+        )
+        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("cumberland", 10))
+        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("vegetarian", 0))
+        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("lincolnshire", 9))
+        conn.commit()
 
-    yield conn
-    conn.close()
+        yield conn
 
 
 @pytest.fixture(scope="function")
