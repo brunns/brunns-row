56bb8c2e575680a9a7a2d51891b6f8052cc2e2da
Simon Brunning <simon@brunningonline.net>
2026-05-01 10:00:51 +0100

    Release v2.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, Simon Brunning"
 author = "Simon Brunning"
 
 # The short X.Y version
-version = "2.0"
+version = "2.2"
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.2.0"
 
 
 # -- General configuration ---------------------------------------------------
