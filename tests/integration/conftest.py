2db07d7741f36c2678d2ba4961b9ae9a4e2f3d9e
Simon Brunning <simon@brunningonline.net>
2026-06-18 11:00:05 +0100

    Pointless test optimisation.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,9 @@
 import logging
 import sqlite3
+from collections.abc import Generator
 from contextlib import closing
 from io import StringIO
+from sqlite3 import Connection
 
 import pytest
 
@@ -9,20 +11,19 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="package")
-def db():
+def db() -> Generator[Connection]:
     with closing(sqlite3.connect(":memory:")) as conn:
         cursor = conn.cursor()
 
         cursor.execute("CREATE TABLE sausages (kind VARCHAR NOT NULL PRIMARY KEY, rating INT NOT NULL);")
-        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("cumberland", 10))
-        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("vegetarian", 0))
-        cursor.execute("INSERT INTO sausages VALUES (?, ?);", ("lincolnshire", 9))
+        sausages = [("cumberland", 10), ("vegetarian", 0), ("lincolnshire", 9)]
+        cursor.executemany("INSERT INTO sausages VALUES (?, ?);", sausages)
         conn.commit()
 
         yield conn
 
 
 @pytest.fixture(scope="function")
-def csv_file():
+def csv_file() -> StringIO:
     data = "kind,rating\ncumberland,10\nlincolnshire,9\nvegetarian,0\n"
     return StringIO(data)
